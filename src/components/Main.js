d42668ec0171bc840cd934611f3f1c7037f7503f
HernandoT <hernandotandra02@gmail.com>
2022-12-01 00:41:40 +0700

    add icon navbar

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,5 +1,13 @@
 /* eslint-disable prettier/prettier */
+/* eslint-disable react-native/no-inline-styles */
 /* eslint-disable react/react-in-jsx-scope */
+import {
+  faGear,
+  faHeart,
+  faHome,
+  faPerson,
+} from '@fortawesome/free-solid-svg-icons';
+import {FontAwesomeIcon} from '@fortawesome/react-native-fontawesome';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import Favorites from '../pages/Favorites';
 import Home from '../pages/Home';
@@ -11,7 +19,37 @@ const Tab = createBottomTabNavigator();
 const Main = () => {
   return (
     <Tab.Navigator
-      screenOptions={tabOptions}>
+      screenOptions={
+        ({route}) => ({
+          tabBarIcon: ({focused}) => {
+            const activeColor = focused ? 'blue' : 'grey';
+            switch (route.name) {
+              case 'Home':
+                return (
+                  <FontAwesomeIcon icon={faHome} style={{color: activeColor}} />
+                );
+              case 'Favorites':
+                return (
+                  <FontAwesomeIcon
+                    icon={faHeart}
+                    style={{color: activeColor}}
+                  />
+                );
+              case 'Profile':
+                return (
+                  <FontAwesomeIcon
+                    icon={faPerson}
+                    style={{color: activeColor}}
+                  />
+                );
+              case 'Setting':
+                return (
+                  <FontAwesomeIcon icon={faGear} style={{color: activeColor}} />
+                );
+            }
+          },
+        })
+      }>
       <Tab.Screen name="Home" component={Home} options={{headerShown: false}} />
       <Tab.Screen
         name="Favorites"
@@ -30,10 +68,6 @@ const Main = () => {
       />
     </Tab.Navigator>
   );
-};
-
-const tabOptions = {
-  tabBarStyle: {height: 60},
 };
 
 export default Main;
